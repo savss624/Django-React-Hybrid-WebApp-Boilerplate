5fc4df8f23cee1eb68e394d67f615fdc1e0a13b1
Parth Srivastava <60739970+savss624@users.noreply.github.com>
2022-09-10 20:17:43 +0530

    actions test fix

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -102,7 +102,7 @@ class PrivateUserApiTests(TestCase):
 
     def setUp(self):
         self.user = create_user(
-            email="test@example.com", password="testpas123", name="Test Name"
+            email="test@example.com", password="testpass123", name="Test Name"
         )
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
